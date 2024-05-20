a7e855c76dd0a8b442f34ce610b5b44be74f7fc5
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2024-05-20 12:45:44 -0600

    Modificación de la BD
    Se aumentan las tablas de ocupaciones, programas y voluntarios, preparando el uso de registro de voluntario.

--- baseDeDatos.js
+++ baseDeDatos.js
@@ -111,12 +111,75 @@ db.serialize(() => {
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
   )`);
 });
-
 /*
   Tipo token
   1 -> Token de creación de cuenta
   2 -> Token de recuperación de cuenta
 */
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS ocupaciones (
+    id_ocupacion INTEGER PRIMARY KEY AUTOINCREMENT,
+    ocupacion TEXT NOT NULL
+  )`);
+});
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS programas (
+    id_programa INTEGER PRIMARY KEY AUTOINCREMENT,
+    programa NOT NULL
+  )`);
+
+  // Insertar datos de roles si la tabla está vacía
+  db.get("SELECT COUNT(*) AS count FROM programas", (err, row) => {
+    if (err) {
+      console.error('Error al verificar tabla de programas:', err);
+    } else {
+      if (row.count === 0) {
+        const programas = [
+          { programa: 'Entrada' },
+          { programa: 'Integra' },
+          { programa: 'D.A.S.' }, // Desarrollando Acciones Solidarias
+          { programa: 'C.V.C' }  // Centro de Vida Cristiana
+        ];
+        const stmt = db.prepare("INSERT INTO programas (programa) VALUES (?)");
+        programas.forEach((programa) => {
+          stmt.run(programa.programa);
+        });
+        stmt.finalize();
+        console.log('Datos de programas insertados correctamente');
+      }
+    }
+  });
+});
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS voluntarios (
+    id_voluntario INTEGER PRIMARY KEY AUTOINCREMENT,
+    es_interno INTEGER NOT NULL,
+    estado INTEGER NOT NULL,
+    fecha_captacion TEXT NOT NULL,
+    fecha_alta TEXT NOT NULL,
+    nombre_v TEXT NOT NULL,
+    apellido_paterno_v TEXT NOT NULL,
+    apellido_materno_v TEXT NOT NULL,
+    identificacion TEXT NOT NULL,
+    fecha_nacimiento TEXT NOT NULL,
+    telefono_v TEXT NOT NULL,
+    correo_v TEXT NOT NULL,
+    id_ocupacion INTEGER NOT NULL,
+    id_derivacion INTEGER,
+    id_valoracion INTEGER,
+    primeros_contactos INTEGER,
+    informe_valoracion INTEGER,
+    fecha_baja TEXT,
+    observaciones TEXT,
+    FOREIGN KEY (id_ocupacion) REFERENCES ocupaciones(id_ocupacion),
+    FOREIGN KEY (id_derivacion) REFERENCES programas(id_programa),
+    FOREIGN KEY (id_valoracion) REFERENCES programas(id_programa)
+  )`);
+});
+/* En sqlite3 las fechas se guardan como texto pero se pueden formatear la consulta con strftime('%d-%m-%Y %H:%M:%S', atributo en la BD). la fecha de captación se almacenará de forma automática */
 
 // Exportar la conexión a la base de datos para ser utilizada en otros archivos
 module.exports = db;
